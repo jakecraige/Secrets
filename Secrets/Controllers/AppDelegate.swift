8fcee2927969848f8dc03e8a19312ab12bcff90f
Jake Craige <james.craige@gmail.com>
2015-04-08 14:53:00 -0700

    Initial Commit

---
+++ Secrets/Controllers/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  Secrets
+//
+//  Created by James Craige on 4/8/15
+//  Copyright (c) 2015 thoughtbot. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
